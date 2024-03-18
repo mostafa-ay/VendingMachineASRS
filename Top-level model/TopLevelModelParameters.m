16e724983e540aaf63d454140a5035715ce6cd55
Mostafa Mahmoud <63523800+mostafa-ay@users.noreply.github.com>
2024-03-18 02:15:27 +0200

    Add files via upload

---
+++ Top-level model/TopLevelModelParameters.m
@@ -0,0 +1,18 @@
+%% Parameters for the equation of motion of the belt conveyor 
+clc; clear;
+r=0.0194;
+k1=2.6552e5;
+k2=2.6552e5;
+k3=11.777e4;
+B1=0.0042;
+B2=0.0084;
+b=40.6518;
+J1=7e-5;
+J2=8e-5;
+m=2; 
+
+%% DC_Motor Parameters
+Kt=23e-3;     %tourque constant
+Kb=23e-3;     %back EMF constant
+L=0.1;        %armarture coil inductance
+R=0.71;       %armature coil resistance
